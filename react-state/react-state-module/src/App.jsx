70b8813d327b1ed4fdd1d5c03544e8417c00390f
Aman Gupta <guptakunalx@gmail.com>
2023-05-29 16:35:51 +0530

    Outputting Lists

--- react-state/react-state-module/src/App.jsx
+++ react-state/react-state-module/src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 // what is state?
 //  => component data that may change overtime
 
@@ -5,6 +7,12 @@ import { useState } from "react";
 
 function App() {
 	const [name, setName] = useState("Ryu");
+	// eslint-disable-next-line no-unused-vars
+	const [events, setEvents] = useState([
+		{ title: "mario's birthday bash", id: 1 },
+		{ title: "bowser's live stream", id: 2 },
+		{ title: "race on moo moo farm", id: 3 },
+	]);
 
 	const handleClick = () => {
 		setName("Mario");
@@ -14,6 +22,13 @@ function App() {
 		<>
 			<h1>My name is {name}</h1>
 			<button onClick={handleClick}>Change Name</button>
+			{events.map((event, index) => {
+				return (
+					<div key={event.id}>
+						<h2>{index + 1}. {event.title}</h2>
+					</div>
+				);
+			})}
 		</>
 	);
 }
