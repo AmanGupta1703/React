31d26848593e25d01271ef0fc343df5ac7feb5b0
Aman Gupta <guptakunalx@gmail.com>
2023-05-30 09:52:19 +0530

    lesson-30

--- react-componets-props-module/src/App.jsx
+++ react-componets-props-module/src/App.jsx
@@ -8,7 +8,7 @@ import Title from "./components/title";
 import Modal from "./components/Modal";
 
 function App() {
-	const [showModal, setShowModal] = useState(true);
+	const [showModal, setShowModal] = useState(false);
 	const [showEvents, setShowEvents] = useState(true);
 	// eslint-disable-next-line no-unused-vars
 	const [events, setEvents] = useState([
@@ -27,6 +27,10 @@ function App() {
 		setShowModal(!showModal);
 	};
 
+	const handleShow = () => {
+		setShowModal(true);
+	};
+
 	const subtitle = "All the latest events in MarioLand!";
 
 	return (
@@ -38,9 +42,9 @@ function App() {
 				</div>
 			)}
 			{!showEvents && (
-				<>
+				<div>
 					<button onClick={() => setShowEvents(true)}>show events</button>
-				</>
+				</div>
 			)}
 			{showEvents &&
 				events.map((event, index) => {
@@ -55,12 +59,15 @@ function App() {
 						</React.Fragment>
 					);
 				})}
-			{showModal &&
+			{showModal && (
 				<Modal handleClose={handleClose}>
 					<h2>10% Off Coupon Code!!</h2>
 					<p>Use the code NINJA10 at the checkout.</p>
 				</Modal>
-			}
+			)}
+			<div>
+				<button onClick={handleShow}>show modal</button>
+			</div>
 		</div>
 	);
 }
