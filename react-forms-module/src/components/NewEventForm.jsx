7a922ccb4efe37faa63764f59192484c24961e6c
Aman Gupta <guptakunalx@gmail.com>
2023-05-31 08:33:06 +0530

    lesson-42

--- react-forms-module/src/components/NewEventForm.jsx
+++ react-forms-module/src/components/NewEventForm.jsx
@@ -3,22 +3,37 @@ import { useState } from "react";
 import "./NewEventForm.css";
 
 export default function NewEventForm() {
-    const [ title, setTitle ] = useState("");
-    const [ date, setDate ] = useState("");
+	const [title, setTitle] = useState("");
+	const [date, setDate] = useState("");
+
+	const resetForm = () => {
+		setTitle("");
+		setDate("");
+	};
 
 	return (
 		<form className="new-event-form">
 			<label>
 				<span>Event TItle: </span>
-				<input type="text" onChange={(e) => setTitle(e.target.value)} />
+				<input
+					type="text"
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
 			</label>
 			<label>
 				<span>Event Date: </span>
-				<input type="date" onChange={(e) => setDate(e.target.value)} />
+				<input
+					type="date"
+					value={date}
+					onChange={(e) => setDate(e.target.value)}
+				/>
 			</label>
-            <p>{title}. {date}</p>
-			<button>Submit</button> 
+			<p>
+				{title}. {date}
+			</p>
+            <p onClick={resetForm} style={ { cursor: "pointer" } }>reset the form.</p>
+			<button>Submit</button>
 		</form>
 	);
-
 }
