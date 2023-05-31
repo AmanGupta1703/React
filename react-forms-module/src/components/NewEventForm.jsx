2b492b3fa0947638bc74a8c1c9e6e88db89d445f
Aman Gupta <guptakunalx@gmail.com>
2023-05-31 10:05:15 +0530

    lesson-45

--- react-forms-module/src/components/NewEventForm.jsx
+++ react-forms-module/src/components/NewEventForm.jsx
@@ -1,45 +1,38 @@
 /* eslint-disable */
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./NewEventForm.css";
 
-export default function NewEventForm( { addEvent } ) {
-	const [title, setTitle] = useState("");
-	const [date, setDate] = useState("");
+export default function NewEventForm({ addEvent }) {
+	const title = useRef();
+	const date = useRef();
 
 	const resetForm = () => {
-		setTitle("");
-		setDate("");
+		title.current.value;
+		date.current.value;
 	};
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-        const event = {
-            title,
-            date,
-            id: Math.floor( Math.random() * 10000 )
-        };
-        addEvent(event);
-        resetForm();
-    }
+		const event = {
+			title: title.current.value,
+			date: date.current.value,
+			id: Math.floor(Math.random() * 10000),
+		};
+		console.log(event);
+		addEvent(event);
+		resetForm();
+	};
 
 	return (
 		<form className="new-event-form" onSubmit={handleSubmit}>
 			<label>
 				<span>Event TItle: </span>
-				<input
-					type="text"
-					value={title}
-					onChange={(e) => setTitle(e.target.value)}
-				/>
+				<input type="text" ref={title} />
 			</label>
 			<label>
 				<span>Event Date: </span>
-				<input
-					type="date"
-					value={date}
-					onChange={(e) => setDate(e.target.value)}
-				/>
+				<input type="date" ref={date} />
 			</label>
 			<button>Submit</button>
 		</form>
