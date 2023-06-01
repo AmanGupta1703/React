3b07a82a15ffff41fc37912f48f02b259adccb36
Aman Gupta <guptakunalx@gmail.com>
2023-06-01 08:11:35 +0530

    lesson-47

--- learning-journal/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import "./Footer.css";
-export default function Footer() {
-	return <footer className="footer">
-        <p className="footer__title">My Learning Journal</p>
-        <p className="footer__copywrite-info">Copyright ©2022</p>
-    </footer>;
-}
--- react-forms-module/README.md
+++
@@ -1,2 +0,0 @@
-`component-specific-css`
-- They are not just specific to that component but can effect the other elements on the webpage having same classes or element 
