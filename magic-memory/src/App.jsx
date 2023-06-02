df100a0606c85746aa855159763271047c62f6fd
Aman Gupta <guptakunalx@gmail.com>
2023-06-02 17:16:43 +0530

    lesson-64-L7

--- magic-memory/src/App.jsx
+++ magic-memory/src/App.jsx
@@ -4,12 +4,12 @@ import "./App.css";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-	{ src: "/img/helmet-1.png" },
-	{ src: "/img/potion-1.png" },
-	{ src: "/img/ring-1.png" },
-	{ src: "/img/scroll-1.png" },
-	{ src: "/img/shield-1.png" },
-	{ src: "/img/sword-1.png" },
+	{ src: "/img/helmet-1.png", matched: false },
+	{ src: "/img/potion-1.png", matched: false },
+	{ src: "/img/ring-1.png", matched: false },
+	{ src: "/img/scroll-1.png", matched: false },
+	{ src: "/img/shield-1.png", matched: false },
+	{ src: "/img/sword-1.png", matched: false },
 ];
 
 function App() {
@@ -44,14 +44,23 @@ function App() {
 	useEffect(() => {
 		if (choiceOne && choiceTwo) {
 			if (choiceOne.src === choiceTwo.src) {
-				console.log("Macth");
+				setCards((prevCards) => {
+					return prevCards.map((card) => {
+						if (card.src === choiceOne.src) {
+							return { ...card, matched: true };
+						} else {
+							return card;
+						}
+					});
+				});
 				resetTurn();
 			} else {
-				console.log("No Match");
 				resetTurn();
 			}
 		}
 	}, [choiceOne, choiceTwo]);
+
+	console.log(cards);
 
 	return (
 		<div className="App">
