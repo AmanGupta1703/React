2e1cf8a163effe29974f1801978f8a71dbac779d
Aman Gupta <guptakunalx@gmail.com>
2023-06-02 17:50:26 +0530

    lesson-65

--- magic-memory/src/components/SingleCard.jsx
+++ magic-memory/src/components/SingleCard.jsx
@@ -1,20 +1,22 @@
 /* eslint-disable */
 import "./SingleCard.css";
-export default function SingleCard({ card: { id, src }, handleChoice }) {
 
-	const handleClick = (card) => {
-		handleChoice({ id, src });
-	}
+export default function SingleCard({ card, handleChoice, flipped }) {
+	const handleClick = () => {
+		handleChoice(card);
+	};
 
 	return (
 		<div className="card">
-			<img className="front" src={src} alt="card front" />
-			<img
-				className="back"
-				onClick={handleClick}
-				src="/img/cover.png"
-				alt="crad back"
-			/>
+			<div className={flipped ? "flipped" : ""}>
+				<img className="front" src={card.src} alt="card front" />
+				<img
+					className="back"
+					src="/img/cover.png"
+					onClick={handleClick}
+					alt="cover"
+				/>
+			</div>
 		</div>
 	);
 }
