569c0aba022132797be904f1aea42696229d99df
Aman Gupta <guptakunalx@gmail.com>
2023-06-02 19:45:09 +0530

    lesson-68-final

--- magic-memory/src/App.jsx
+++ magic-memory/src/App.jsx
@@ -25,6 +25,8 @@ function App() {
 			.sort(() => Math.random() - 0.5)
 			.map((card) => ({ ...card, id: Math.random() }));
 
+        setChoiceOne(null);
+        setChoiceTwo(null);
 		setCards(shuffledCards);
 		setTurns(0);
 	};
@@ -57,6 +59,11 @@ function App() {
 		}
 	}, [choiceOne, choiceTwo]);
 
+    // start a game automatically
+    useEffect(() => {
+        shuffleCards();
+    }, []);
+
 	// reset choice and increase turn
 	const resetTurn = () => {
 		setChoiceOne(null);
@@ -80,6 +87,7 @@ function App() {
 					/>
 				))}
 			</div>
+            <p>Turns: {turns}</p>
 		</div>
 	);
 }
